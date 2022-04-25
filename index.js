a3474b856465dbd0c5094e0af175a8a9a18578ed
Tunjii10 <oluwatunji7@gmail.com>
2022-04-25 19:28:55 +0100

    frontend setup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require("express");
-var cors = require("cors");
+const path = require("path");
+
 var app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("api/v1/summarize");
+//app.use("api/v1/summarize");
 
 app.listen(port, () => console.log("server running on port", port));
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      orange: "#ff7849",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
