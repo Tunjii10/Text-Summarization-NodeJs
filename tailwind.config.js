40b217dd63aa8c6ce2eba875f2b2b0c8fb5baf1a
Tunjii10 <oluwatunji7@gmail.com>
2022-04-28 11:43:09 +0100

    frontend-design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
     extend: {},
   },
   plugins: [],
